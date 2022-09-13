6eec3d3ca0fa90d64bbee28113db5d7769edfb9b
hiyu12 <107801575+hiyu12@users.noreply.github.com>
2022-09-13 17:20:59 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,12 +1,10 @@
-providers "aws"{
-  region= ${region1}
-}
-
 resource "aws_instance" "ak" {
-  ami= ${ami1}
-  type= ${type1}
+  region= "ap-south-1"
+  ami= "ami-07eaf27c7c4a884cf"
+  instance_type= "t2.micro"
   tags= {
      Name= "ak"
   }
-  
+
 }
+~  
