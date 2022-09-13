6a2191b9494653a7c16fec37077599d1fb1f8719
hiyu12 <107801575+hiyu12@users.noreply.github.com>
2022-09-13 17:41:07 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "ak" {
-  ami= "ami-07eaf27c7c4a884cf"
-  instance_type= "t2.micro"
-  count= 1
-  tags= {
-     Name= "ak"
+resource "aws_instance" "web" {
+  ami = "ami-07eaf27c7c4a884cf"
+  instance_type = "t2.micro"
+  count = 1
+  tags = {
+     Name = "ak"
   }
 } 
